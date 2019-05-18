e22f0904342f3655115e92b5425c422a278c6430
shubhammodi004 <shubhammodi004@example.com>
2019-05-18 15:40:42 +0530

    now this shud be happening

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,13 +3,13 @@ import { FormattedMessage } from 'react-intl';
 
 import withLayout from '../layout';
 import Link from '../components/Link';
-// import Landing from '../components/About/Landing';
-// import Sections from '../components/About/Sections';
+import Landing from '../components/AboutComponent/AboutLanding';
+import Sections from '../components/AboutComponent/Sections';
 
 const AboutPage = () => (
   <>
-    {/* <Landing />
-    <Sections /> */}
+    <Landing />
+    <Sections />
   </>
 );
 
