97fb72c075f676cd7aff2e64cdcd8e96f3aa3efb
shubhammodi004 <shubhammodi004@example.com>
2019-06-04 21:09:46 +0530

    done with footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,6 +6,9 @@ import { Link } from 'gatsby';
 import Instagram from './Instagramfooter';
 import Facebook from './Facebookfooter';
 import Whatsapp from './Whatsappfooter';
+import Bluefooter from './Bluefooter';
+import Yellowfooter from './Yellowfooter';
+import Logo from './Logo';
 
 // import css
 import './Footer.css';
@@ -23,7 +26,51 @@ class Footer extends Component {
                     </div>
                 </div>
                 <div className="footer-bottom">
+                    <div className="blue-footer">
+                        <Bluefooter />
+                    </div>
+                    <div className="yellow-footer">
+                        <Yellowfooter />
+                    </div>
+                    <div className="row container">
+                        <div className="col-md-3">
+                            <div className="logo-footer">
+                                <Logo />
+                            </div>
+                        </div>
+                        <div className="col-md-4 mt-2">
+                            <h5>Quick Links</h5>
+                            <div className="footer-quicklinks">
+                                <div className="quicklinks">
+                                    <Link to="/" style={{ textDecoration: `none` }}>Home</Link>
+                                    <Link to="/AboutPage/" style={{ textDecoration: `none` }}>About</Link>
+                                    <Link to="/ProductPage/" style={{ textDecoration: `none` }}>Product</Link>
+                                    <Link to="/PurchasePage/" style={{ textDecoration: `none` }}>Purhcase</Link>
 
+                                </div>
+                                <div className="quicklinks">
+                                    <Link to="/SkillsPage/" style={{ textDecoration: `none` }}>Skills</Link>
+                                    <Link to="/KnowledgePage/" style={{ textDecoration: `none` }}>Knowledge Bank</Link>
+                                    <Link to="/FAQ/" style={{ textDecoration: `none` }}>FAQ</Link>
+                                    <Link to="/contact/" style={{ textDecoration: `none` }}>Contact us</Link>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="col-md-3 mt-2">
+                            <h5>Legal</h5>
+                            <div className="quicklinks">
+                                <Link to="/" style={{ textDecoration: `none` }}>Privacy Policy</Link>
+                                <Link to="/AboutPage/" style={{ textDecoration: `none` }}>Complaint Registration</Link>
+                                <Link to="/ProductPage/" style={{ textDecoration: `none` }}>Warranty & Delivery Policy</Link>
+                                <Link to="/PurchasePage/" style={{ textDecoration: `none` }}>Support</Link>
+                            </div>
+                        </div>
+                        <div className="col-md-2 mt-2">
+                            <h5>Let's Talk</h5>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         )
