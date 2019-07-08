eaa4ad204e6cf2840d11395a0312339fd17b8515
shubhammodi004 <shubhammodi004@example.com>
2019-07-08 13:27:54 +0530

    cooler stuff

--- src/components/Home/Landing/index.jsx
+++ src/components/Home/Landing/index.jsx
@@ -18,7 +18,6 @@ class Index extends Component {
                 <div className="main-title">
                     <h1><FormattedMessage id="home.Revolutionizing the way content is delivered in school" /></h1>
                     <p className="light"><FormattedMessage id="home.Digital education to all class of students" /></p>
-                    <p><FormattedMessage id="home.Educrate was created by keeping in mind that digital/smart classroom concept should reach all the students, every student should have equal access to the technological advancements in the field of education and moreover audio visual content should reach them, at minimal cost." /></p>
                     <a href="#product-home" className="yellowButtons"><FormattedMessage id="home.FIND OUT HOW" /></a>
                 </div>
                 <div className="main-title-imageSection">
